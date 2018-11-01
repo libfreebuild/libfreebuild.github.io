487093046da0204e50f5aeeb9143bca822cac383
libfreebuild <artosc890l@gmx.es>
2018-11-01 08:58:23 +0100

    website

--- php/cn.php
+++
@@ -1,9 +0,0 @@
-<?php
-$conexion = mysql_connect("localhost","root", ""; "bd_prueba");
-if(!$conexion){
-	echo 'Error al conectar a la base de datos';
-}
-else{
-	echo 'Conectado al a base de datos';
-}
-?>
